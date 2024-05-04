f0e52bc399568b5544d7f40f7ac6cd66a80fdf67
NGGJamie <6971628+NGGJamie@users.noreply.github.com>
2024-05-04 17:01:49 -0500

    tools

--- dependencies.sh
+++ dependencies.sh
@@ -1,26 +1,33 @@
-#!/bin/bash
+#!/bin/sh
 
 #Project dependencies file
 #Final authority on what's required to fully build the project
 
 # byond version
-# Extracted from the Dockerfile. Change by editing Dockerfile's FROM command.
-LIST=($(sed -n 's/.*byond:\([0-9]\+\)\.\([0-9]\+\).*/\1 \2/p' Dockerfile))
-export BYOND_MAJOR=${LIST[0]}
-export BYOND_MINOR=${LIST[1]}
-unset LIST
+export BYOND_MAJOR=515
+export BYOND_MINOR=1633
 
 #rust_g git tag
-export RUST_G_VERSION=0.4.5
+export RUST_G_VERSION=3.1.0
 
 #node version
-export NODE_VERSION=12
-
-# PHP version
-export PHP_VERSION=7.2
+export NODE_VERSION=20
+export NODE_VERSION_LTS=20.12.0
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.1
+export SPACEMAN_DMM_VERSION=suite-1.8
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.9.0
+
+#auxlua repo
+export AUXLUA_REPO=tgstation/auxlua
+
+#auxlua git tag
+export AUXLUA_VERSION=1.4.4
+
+#hypnagogic repo
+export CUTTER_REPO=spacestation13/hypnagogic
+
+#hypnagogic git tag
+export CUTTER_VERSION=v3.1.0
--- tools/deploy.sh
+++ tools/deploy.sh
@@ -5,32 +5,40 @@
 #creates a work tree free of everything except what's necessary to run the game
 
 #second arg is working directory if necessary
-if [[ $# -eq 2 ]]; then
-	cd $2
+if [[ $# -eq 2 ]] ; then
+  cd $2
 fi
 
 mkdir -p \
-	$1/maps \
-	$1/icons \
-	$1/sound \
-	$1/config \
-	$1/strings \
-	$1/nano \
-	$1/map_config
+    $1/_maps \
+    $1/icons/effects \
+    $1/icons/mob/clothing \
+    $1/icons/mob/inhands \
+    $1/icons/mob/simple \
+    $1/icons/obj \
+    $1/icons/runtime \
+    $1/sound/runtime \
+    $1/strings \
+    $1/tgui/public \
+    $1/tgui/packages/tgfont/dist
 
 if [ -d ".git" ]; then
-	mkdir -p $1/.git/logs
-	cp -r .git/logs/* $1/.git/logs/
+  mkdir -p $1/.git/logs
+  cp -r .git/logs/* $1/.git/logs/
 fi
 
-cp roguetown.dmb roguetown.rsc $1/
-cp -r maps/* $1/maps/
-cp -r icons/* $1/icons/
-cp -r sound/* $1/sound/
+cp tgstation.dmb tgstation.rsc $1/
+cp -r _maps/* $1/_maps/
+cp -r icons/effects/* $1/icons/effects/
+cp -r icons/mob/clothing/* $1/icons/mob/clothing/
+cp -r icons/mob/inhands/* $1/icons/mob/inhands/
+cp -r icons/mob/simple/* $1/icons/mob/simple/
+cp -r icons/obj/* $1/icons/obj/
+cp -r icons/runtime/* $1/icons/runtime/
+cp -r sound/runtime/* $1/sound/runtime/
 cp -r strings/* $1/strings/
-cp -r config/* $1/config/
-cp -r nano/* $1/nano/
-cp -r map_config/* $1/map_config/
+cp -r tgui/public/* $1/tgui/public/
+cp -r tgui/packages/tgfont/dist/* $1/tgui/packages/tgfont/dist/
 
 #remove .dm files from _maps
 
